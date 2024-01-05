f4daf87022b3c31e629ff820dc47942830ec314d
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-01-05 12:39:13 +0530

    Learn basices of JS.

---
+++ intro_JS/script.js
@@ -0,0 +1,27 @@
+      //define variable
+      let score = 10;
+      const pi = 3.14;
+
+
+      // alert(score);
+      console.log("The score is"+ score);
+
+      //special value
+      console.log("10/0 = "+ 10/0);
+      console.log("-10/0 = "+ -10/0);
+      console.log("0/0 = "+ 0/0);
+
+      //comparison
+      let a = '5';
+      console.log("type of a: "+ typeof a)
+      console.log(a == 5)//it only consider value not type
+      console.log(a === 5)//it consider both value and type
+
+      //user input 
+    //   let name = prompt("what is your name?")
+    //   console.log("hi "+ name)
+
+    // let numOfTicket = prompt("how many ticket do you want?")
+    // console.log("you should pay "+ Number(numOfTicket)*20)
+
+    
