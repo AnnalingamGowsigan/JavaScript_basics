a89334e998cf06bdea1ba4c5ff942fe99ca6f9b7
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-01-07 11:26:57 +0530

    Add Async HTML and JavaScript files.

---
+++ Async/script.js
@@ -0,0 +1,9 @@
+console.log("hello world1")
+console.log("hello world2")
+console.log("hello world3")
+
+setTimeout(() => {console.log("hello world6")}, 6000);
+setTimeout(() => {console.log("hello world5")}, 5000);
+setTimeout(() => {console.log("hello world4")}, 4000);
+
+console.log("bye")
